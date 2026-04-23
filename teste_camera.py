bc6af9f128dac716207b4ceddbfdb65310a3f402
Jvictor2109 <joaobrasil2109@gmail.com>
2026-04-23 17:11:49 +0100

    camera

--- teste_camera.py
+++ teste_camera.py
@@ -2,10 +2,16 @@ import cv2
 from picamera2 import Picamera2
 camera = Picamera2()
 camera.configure(camera.create_preview_configuration(
-    main={"format": "RGB888", "size": (320, 240)}
+    main={"format": "RGB888", "size": (1280, 720)}
 ))
 camera.start()
 
 while True:
     frame = camera.capture_array()
     cv2.imshow("Frame", frame)
+    # falta isto:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+camera.stop()
+cv2.destroyAllWindows()
