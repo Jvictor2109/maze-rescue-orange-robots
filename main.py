d7de5424f2af12f8c182a223fe77407ad6a1f1c7
Jvictor2109 <joaobrasil2109@gmail.com>
2026-04-24 18:50:29 +0100

    ss

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ WEST = 3
 # =====================================================================
 CELL_DISTANCE_CM = 30.0       # Distancia de uma celula em cm (encoder)
 WALL_THRESHOLD_CM = 15.0      # Distancia <= threshold = parede (ultrassonico)
-MOTOR_SPEED = 30           # Velocidade padrao dos motores
+MOTOR_SPEED = 40        # Velocidade padrao dos motores
 DR_POLL_INTERVAL = 0.05       # 50ms entre leituras de encoder
 
 # Convencao standard: N=+Y, E=+X, S=-Y, W=-X
